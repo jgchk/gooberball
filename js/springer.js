3b57feee92080777ae0219fafb895de5f8cd83c5
Jake Cheek <jake@f-m.fm>
2022-01-17 22:00:11 -0500

    update springer

--- js/springer.js
+++ js/springer.js
@@ -1,12 +1,25 @@
 /// <reference path="../types/p5.global-mode.d.ts" />
 
-let dot
+let dots
 let ball
 
 function setup() {
   createCanvas(500, 500)
-  dot = createSpring(createVector(width / 2, height / 2), 0.1, 1, 5, color(255))
-  ball = createSpring(createVector(mouseX, mouseY), 1, 1, 30, color(255, 0, 0))
+  dots = []
+  for (let i = 0; i < 100; i++) {
+    let r = Math.floor(i / 10)
+    let c = i % 10
+    dots.push(
+      createSpring(
+        createVector(r * (height / 10) + 20, c * (width / 10) + 20),
+        0.1,
+        1,
+        5,
+        color(255)
+      )
+    )
+  }
+  ball = createSpring(createVector(mouseX, mouseY), 1, 1, 100, color(0))
 }
 
 function draw() {
@@ -19,33 +32,37 @@ function draw() {
   const ballDragForce = V.mult(ball.v, -0.9)
   ballForce = V.add(ballForce, ballDragForce)
 
-  let dotForce = createVector(0, 0)
-
-  if (mouseIsPressed) {
-    const clickForce = V.add(dotForce, createVector(10, 0))
-    dotForce = V.add(dotForce, clickForce)
-  }
-
-  const springForce = getSpringForce(dot)
-  dotForce = V.add(dotForce, springForce)
-
-  const ballToDot = V.sub(dot.p, ball.p)
-  const ballToDotMag = ballToDot.mag()
-  const distanceBetween = ballToDotMag - (ball.r + dot.r)
-  if (distanceBetween < 0) {
-    const displacement = V.div(ballToDot, ballToDotMag).mult(-distanceBetween)
-    const pushForce = V.mult(displacement, 1)
-    dotForce = V.add(dotForce, pushForce)
-  }
-
-  const dragForce = V.mult(dot.v, -0.1)
-  dotForce = V.add(dotForce, dragForce)
-
-  updateSpring(dot, dotForce)
   updateSpring(ball, ballForce)
-
-  drawSpring(dot)
   drawSpring(ball)
+
+  for (let i = 0; i < dots.length; i++) {
+    const dot = dots[i]
+
+    let dotForce = createVector(0, 0)
+
+    if (mouseIsPressed) {
+      const clickForce = V.add(dotForce, createVector(10, 0))
+      dotForce = V.add(dotForce, clickForce)
+    }
+
+    const springForce = getSpringForce(dot)
+    dotForce = V.add(dotForce, springForce)
+
+    const ballToDot = V.sub(dot.p, ball.p)
+    const ballToDotMag = ballToDot.mag()
+    const distanceBetween = ballToDotMag - (ball.r + dot.r)
+    if (distanceBetween < 0) {
+      const displacement = V.div(ballToDot, ballToDotMag).mult(-distanceBetween)
+      const pushForce = V.mult(displacement, 0.2)
+      dotForce = V.add(dotForce, pushForce)
+    }
+
+    const dragForce = V.mult(dot.v, -0.1)
+    dotForce = V.add(dotForce, dragForce)
+
+    updateSpring(dot, dotForce)
+    drawSpring(dot)
+  }
 }
 
 const V = {
